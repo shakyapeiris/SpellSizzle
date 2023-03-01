5a8e8907650046e116257c330be083c7b1aa293b
NuwanthaPasindhu <hnpkdias@gmail.com>
2023-03-01 22:05:20 +0530

    Leadeboard

--- controller/leaderBoard.php
+++ controller/leaderBoard.php
@@ -5,19 +5,13 @@ $db = new db();
 $connection = $db->conn;
 $leaderBoard = [];
 $topPlayers = [];
-$query = "SELECT * FROM users HAVING rate >=1000 ORDER BY rate DESC LIMIT 3";
-$run = mysqli_query($connection, $query);
-while ($data = mysqli_fetch_assoc($run)){
-    array_push($leaderBoard,$data);
-}
 
-
-$topPlayerQuery = "SELECT * FROM users HAVING rate >=1000 ORDER BY rate DESC LIMIT 10";
-$topPlayerQueryRun = mysqli_query($connection, $query);
+$topPlayerQuery = "SELECT * FROM users  ORDER BY rate DESC LIMIT 10";
+$topPlayerQueryRun = mysqli_query($connection, $topPlayerQuery);
 while ($data = mysqli_fetch_assoc($topPlayerQueryRun)){
     array_push($topPlayers,$data);
 }
 
-echo json_encode(["leaderBoard"=>$leaderBoard,"topPlayers"=>$topPlayers]);
+echo json_encode(["layers"=>$topPlayers]);
 
 
